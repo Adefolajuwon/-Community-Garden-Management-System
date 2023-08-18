e8d12aa16edeae1eeb7b93ba614dc479f83b1d96
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-18 20:57:31 +0100

    change

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,12 +31,13 @@ async function getSpecificUser(req, res) {
 
 		const user = await User.findOne({ where: { firstname: firstname } });
 
-		if (user) {
-			res.status(200).json({ user: user });
-		} else {
-			// User not found, send a 404 error response.
-			res.status(404).json({ error: 'User not found' });
-		}
+		// if (user) {
+		res.status(200).json({ user: user });
+		// }
+		// } else {
+		// 	// User not found, send a 404 error response.
+		// 	res.status(404).json({ error: 'User not found' });
+		// }
 	} catch (error) {
 		// Handle errors and send an appropriate error response
 		console.error('Error fetching user:', error);
