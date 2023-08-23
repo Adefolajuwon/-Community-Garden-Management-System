fc290404d68590eb16007efdee4b072e1ea61616
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-23 19:29:41 +0100

    change

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,3 +1,11 @@
-const GardenPlot = require('../schemas/garden.schema');
-async function createTask() {}
+const Task = require('../schemas/task.schema');
+async function createTask() {
+	try {
+		const { title, description } = req.body;
+		const task = await Task.create({ title, description });
+		res.status(200).json(task);
+	} catch (error) {
+		console;
+	}
+}
 async function getTask() {}
--- src/schemas/task.schema.js
+++ src/schemas/task.schema.js
@@ -4,6 +4,8 @@ const taskSchema = new mongoose.Schema({
 	title: String,
 	description: String,
 	status: String,
+	assignee: String,
+	dueDate: String,
 });
 
 const Task = mongoose.model('Task', taskSchema);
