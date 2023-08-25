b6f4932c1c82edca6fa19734ab59aaf4df9bf3eb
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-25 19:16:36 +0100

    change

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,60 +1,97 @@
 const app = require('../../app'); // Import your Express app
 const request = require('supertest');
 const User = require('../schemas/user.schema'); // Assuming your User model is in User.js
+const { response } = require('../../app');
 
-describe('POST /api/users', () => {
-	// beforeAll(async () => {
-	// 	// Connect to a test database or set up a mock database
-	// });
+// describe('POST /api/users', () => {
+// 	// beforeAll(async () => {
+// 	// 	// Connect to a test database or set up a mock database
+// 	// });
 
-	// afterAll(async () => {
-	// 	// Close the database connection or clean up after tests
-	// });
+// 	// afterAll(async () => {
+// 	// 	// Close the database connection or clean up after tests
+// 	// });
 
-	it('should create a new user', async () => {
-		const newUser = {
-			username: 'testuser',
-			email: 'test@example.com',
-			bio: 'Test bio',
-			password: 'testpassword',
-		};
+// 	it('should create a new user', async () => {
+// 		const newUser = {
+// 			username: 'testuser',
+// 			email: 'test@example.com',
+// 			bio: 'Test bio',
+// 			password: 'testpassword',
+// 		};
 
-		const response = await request(app)
-			.post('/api/users')
-			.send(newUser)
-			.expect(201);
+// 		const response = await request(app)
+// 			.post('/api/users')
+// 			.send(newUser)
+// 			.expect(201);
 
-		// Assertions to check the response
-		expect(response.body.username).toBe(newUser.username);
-		expect(response.body.email).toBe(newUser.email);
-		expect(response.body.bio).toBe(newUser.bio);
-		// You can add more assertions here based on your schema and response format
-	});
+// 		// Assertions to check the response
+// 		expect(response.body.username).toBe(newUser.username);
+// 		expect(response.body.email).toBe(newUser.email);
+// 		expect(response.body.bio).toBe(newUser.bio);
+// 		// You can add more assertions here based on your schema and response format
+// 	});
 
-	it('should return an error if username is taken', async () => {
-		const existingUser = {
-			username: 'existinguser',
-			email: 'existing@example.com',
-			bio: 'Existing user bio',
-			password: 'existingpassword',
-		};
+// 	it('should return an error if username is taken', async () => {
+// 		const existingUser = {
+// 			username: 'existinguser',
+// 			email: 'existing@example.com',
+// 			bio: 'Existing user bio',
+// 			password: 'existingpassword',
+// 		};
 
-		// Create the existing user before running the test
-		await User.create(existingUser);
+// 		// Create the existing user before running the test
+// 		await User.create(existingUser);
 
-		const newUserWithTakenUsername = {
-			username: existingUser.username, // Use the existing username
-			email: 'new@example.com',
-			bio: 'New user bio',
-			password: 'newpassword',
-		};
+// 		const newUserWithTakenUsername = {
+// 			username: existingUser.username, // Use the existing username
+// 			email: 'new@example.com',
+// 			bio: 'New user bio',
+// 			password: 'newpassword',
+// 		};
 
-		const response = await request(app)
-			.post('/api/users')
-			.send(newUserWithTakenUsername)
-			.expect(505); // Expected status code for username taken error
+// 		const response = await request(app)
+// 			.post('/api/users')
+// 			.send(newUserWithTakenUsername)
+// 			.expect(505); // Expected status code for username taken error
 
-		// Assertions to check the error response
-		expect(response.body.error).toBe('Username taken');
-	});
+// 		// Assertions to check the error response
+// 		expect(response.body.error).toBe('Username taken');
+// 	});
+// });
+it('GET /user/id --> specific user', () => {
+	return response(app)
+		.get('/user/:username')
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.then((response) => {
+			expect(response.body).toEqual(
+				expect.objectContaining({
+					username: expect.any(String),
+					email: expect.any(String),
+					bio: expect.any(String),
+				})
+			);
+		});
 });
+// it('GET /user/id--> 404 ', () => {});
+// it('POST /register --> new user', () => {
+// 	return request(app)
+// 		.post('/todos')
+// 		.send({
+// 			username: 'travis',
+// 			email: expect.any('oadefolajuwon@gmail.com'),
+// 			bio: expect.any('food is greeat'),
+// 		})
+// 		.expect('Content-Type', /json/)
+// 		.expect(201)
+// 		.then((response) => {
+// 			expect(response.body).toEqual(
+// 				expect.objectContaining({
+// 					username: expect.any('travis'),
+// 					email: expect.any('oadefolajuwon@gmail.com'),
+// 					bio: expect.any('food is greeat'),
+// 				})
+// 			);
+// 		});
+// });
