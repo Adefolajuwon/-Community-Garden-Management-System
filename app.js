44407e4fb0769060b5e1eac10960f2b268e036df
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-25 08:18:14 +0100

    change

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const cors = require('cors');
 const { userRouter } = require('./src/routes/user.routes');
 const { gardenRouter } = require('./src/routes/garden.routes');
 const { taskRouter } = require('./src/routes/task.routes');
-app.use(express.json());
+
+// Middleware to set Content-Type header to JSON
+app.use((req, res, next) => {
+	res.setHeader('Content-Type', 'application/json');
+	next();
+});
+
+// app.use(express.json());
 app.use(
 	cors({
 		origin: '*',
--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -25,6 +25,9 @@ async function getTasks(req, res) {
 			return res.status(404).json({ error: 'Garden not found' });
 		}
 		const tasks = garden.tasks;
+		if (!tasks) {
+			res.status(404).json({ error: 'Tasks not found' });
+		}
 		res.status(200).json(tasks);
 	} catch (error) {
 		console.error('Error fetching tasks:', error);
