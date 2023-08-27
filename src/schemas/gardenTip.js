79d5befce1d7d162e1637a4f6a6a2c94f3c878ec
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-27 23:08:41 +0100

    refactored the get task function

--- src/schemas/gardenTip.js
+++ src/schemas/gardenTip.js
@@ -5,7 +5,6 @@ const gardenTipSchema = new mongoose.Schema({
 	tip: String,
 });
 
-// Create the model
 const GardenTip = mongoose.model('gardenTip', gardenTipSchema);
 
 module.exports = GardenTip;
