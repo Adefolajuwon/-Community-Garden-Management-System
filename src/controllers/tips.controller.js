9452835c60f7da9c02171dd4cfe5071e199ba592
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-08-27 16:59:32 +0100

    refactored the get task function

--- src/controllers/tips.controller.js
+++ src/controllers/tips.controller.js
@@ -2,16 +2,25 @@ const GardenTip = require('../schemas/gardenTip');
 async function getTips(req, res) {
 	try {
 		const countDocument = await GardenTip.countDocuments();
-		const randomNumber = Math.floor(Math.random * countDocument);
+		const randomNumber = Math.floor(Math.random() * countDocument); // Corrected Math.floor usage
 		const randomDocument = await GardenTip.findOne()
 			.skip(randomNumber)
 			.limit(1);
 		if (!randomDocument) {
 			res.status(404).json({ error: 'Document not found' });
+		} else {
+			res.status(200).json(randomDocument);
 		}
-		res.json(200).json(randomDocument);
 	} catch (error) {
-		res.status(200).json(error);
+		res.status(500).json(error);
 	}
 }
-module.exports = { getTips };
+async function getAllTips(req, res) {
+	try {
+		const garden = await GardenTip.find({}).limit(10);
+		res.status(200).json(garden);
+	} catch (error) {
+		res.status(501).json(error);
+	}
+}
+module.exports = { getTips, getAllTips };
--- src/routes/tips.routes.js
+++ src/routes/tips.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { getTips } = require('../controllers/tips.controller');
+const { getTips, getAllTips } = require('../controllers/tips.controller');
 const tipRouter = express.Router();
 tipRouter.get('/tips', getTips);
+tipRouter.get('/all-tips', getAllTips);
+
 module.exports = { tipRouter };
