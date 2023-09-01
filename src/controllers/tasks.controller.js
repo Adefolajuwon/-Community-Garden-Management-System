434f6d4c2c2a6fa31eb004b90dc1cb7dd015ade0
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-01 05:33:53 +0100

    change

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -15,7 +15,7 @@ async function createTask(req, res) {
 			assignee,
 			dueDate: oneWeekLater,
 		});
-		// console.log(oneWeekLater);
+		console.log(oneWeekLater);
 		await task.populate('assignee');
 		const garden = await GardenPlot.findById(gardenId);
 		garden.tasks.push(task._id);
