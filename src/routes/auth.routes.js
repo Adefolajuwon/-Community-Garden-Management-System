9e9cf898f4c6abb69b30233ce9e3217abbbaddcb
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-04 09:34:10 +0100

    change

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -14,29 +14,4 @@ const {
 const authRouter = express.Router();
 // Route to handle sending mails
 
-authRouter.use(
-	session({
-		secret: process.env.SESSION_TOKEN,
-		resave: false,
-		saveUninitialized: false,
-	})
-);
-authRouter.use(passportInstance.initialize());
-authRouter.use(passportInstance.session());
-
-authRouter.get(
-	'/google',
-	passportInstance.authenticate('google', { scope: ['email', 'profile'] })
-);
-authRouter.get(
-	'/google/callback',
-	passportInstance.authenticate('google', { failureRedirect: '/register' }),
-	controllerAuthGoogle
-);
-authRouter.post('/sendemail', sendEmail);
-
-authRouter.post('/local/register', controllerInsertUser);
-authRouter.get('/verify', controllerFetchLoggedInUser);
-authRouter.post('/local/login', controllerVerifyUserCredentials);
-
 module.exports = authRouter;
