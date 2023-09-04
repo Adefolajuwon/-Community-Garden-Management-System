712237e005c25f241673cad731c7332b6718d3f6
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-04 18:20:20 +0100

    change

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,15 +3,28 @@ const express = require('express');
 const session = require('express-session');
 
 const passportInstance = require('../lib/passport');
-const {
-	controllerAuthGoogle,
-	controllerInsertUser,
-	controllerVerifyUserCredentials,
-	controllerFetchLoggedInUser,
-	sendEmail,
-} = require('../controllers/user.controller');
+// const {
+// 	controllerAuthGoogle,
+// 	controllerInsertUser,
+// 	controllerVerifyUserCredentials,
+// 	controllerFetchLoggedInUser,
+// 	sendEmail,
+// } = require('../controllers/user.controller');
 
 const authRouter = express.Router();
 // Route to handle sending mails
+authRouter.use(
+	session({
+		secret: process.env.SESSION_TOKEN,
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+authRouter.use(passportInstance.initialize());
+authRouter.use(passportInstance.session());
 
+authRouter.get(
+	'/google',
+	passportInstance.authenticate('google', { scope: ['email', 'profile'] })
+);
 module.exports = authRouter;
