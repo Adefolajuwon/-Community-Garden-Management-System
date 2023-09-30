5c4edf24b3de9a5478553fafad1b17ce3e2e1b4c
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-09-30 17:35:10 +0100

    added encryption

--- src/utils/encryption.js
+++ src/utils/encryption.js
@@ -1,21 +1,27 @@
 const algorithm = 'aes-256-cbc';
 const crypto = require('crypto');
-
+//password = 4466444
+//input = book
 function encrypt(input, password) {
-	let m = crypto.createHash('md5');
-	m.update(password);
-	let key = m.digest('hex');
+	try {
+		let passwordHash = crypto.createHash('md5');
+		passwordHash.update(password);
+		let key = passwordHash.digest('hex');
 
-	m = crypto.createHash('md5');
-	m.update(password + key);
-	let iv = m.digest('hex');
+		let passwordKey = crypto.createHash('md5');
+		passwordKey.update(password + key);
+		let iv = passwordKey.digest('hex');
 
-	let data = Buffer.from(input, 'utf8').toString('binary');
+		let data = Buffer.from(input, 'utf8').toString('binary');
 
-	let cipher = crypto.createCipheriv(algorithm, key, iv.slice(0, 16));
-	let encrypted =
-		cipher.update(data, 'binary', 'binary') + cipher.final('binary');
-	let encoded = Buffer.from(encrypted, 'binary').toString('base64');
-	return encoded;
+		let cipher = crypto.createCipheriv(algorithm, key, iv.slice(0, 16));
+		let encrypted =
+			cipher.update(data, 'binary', 'binary') + cipher.final('binary');
+		let encoded = Buffer.from(encrypted, 'binary').toString('base64');
+		return encoded;
+	} catch (error) {
+		console.error('Encryption error:', error.message);
+		return null; //
+	}
 }
 module.exports = { encrypt };
